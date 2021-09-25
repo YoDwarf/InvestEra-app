61609874b96f9a552b20c9dc18800b73804694dd
YoDwarf <cross-bearer2010.net@yandex.ru>
2021-09-25 13:37:34 +0300

    Update init.R

--- init.R
+++ init.R
@@ -1,9 +1,11 @@
-my_packages = c("shinyMobile", "magrittr")
+my_packages = "magrittr"
 
 install_if_missing = function(p) {
   if(p %in% rownames(installed.packages())) {
     install.packages(p)
   }
 }
+
+install.packages("shinyMobile_0.9.1.tar.gz", repos=NULL, type="source")
 
 invisible(sapply(my_packages, install_if_missing))
