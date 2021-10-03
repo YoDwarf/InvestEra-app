7b103fb34cd113cc340ea3acf2d712629d57a4c0
YoDwarf <cross-bearer2010.net@yandex.ru>
2021-10-03 15:08:48 +0300

    Update app.R

--- app.R
+++ app.R
@@ -2,6 +2,13 @@ library(shiny)
 library(shinyMobile)
 library(magrittr)
 
+image <- c(
+    "https://lh3.googleusercontent.com/ZclHcYqOan02wB2E3JtdG3sBcAK0HHwGXbZS3fDfIlktpIBUO7Lko6pUHgNT5SIzLDhdJXfavd6BEjYhk7tYXjEadEhL50pIMQ4Wvl5awvykWMsLDHKR0I4hrfaxcSty39TNIjKG49WKh64CANB8kznqnsz2wIOPr8jqwQHUzDkKJ8wJEujxubhormhYpIbXGc0ZBa_JBwOFljc7Lat2I8j21d5uhVCtg3XnN4T6Jkl1Kh4VMJfqjY832R-ndrSKpL0aTpYfeIdaTzPGBH_M_-MqxqK-dWEfI9Uw7Aqa18-gD_LfxqXYoencw32yVGprRWQ5mda8U93A5WKhhS6q_Oai96DMDM6mN9VfhvagiO27QMRGsOQd14zELamU1DlF0DwEOpow6RBpVxKUmea7pBIY0SZxnyEB5PLCo0I3ANWEk-eppjRinwZmohWJBxajC3zA_N56p0YgLyKj6uqNh5cQpJYA-IQKlJW73JIl5N9MEGY___7koVbycErxcELbTEvr272lyQsDBUaP8lTTRNf_H5Q3ndSOt7kDAlL7gHZXkXOEIpgmCW9WaAIfTGhHHnQj0OzeN9TVLkp8eTEvFhh9kcUqwBmbnboCrtVLwqynKH1gT-cIGDq3WjI1eEClI1qf9EGVnV8T8_1AsgfgSlL5dqaPQStqSEIWiL-s9wxyAJrsj7jq-z4VbdHKEdNyGfabQ5QxY1VKj3fo=s640-no?authuser=1",
+    "https://lh3.googleusercontent.com/TATO1D2BJiJjGKAXvGR_9JVNWVdKecT7PwikisvK-6vgU-PMGmH6S2j4xwTLXlm0yM67YSBnr-mAaWLNMqy9drE5YEtQwTSYvJmyJHoxm_28upIeUsQ1aOWfZi0Fsh0GNJGLI8iIDiJnL9oLQbWQwXsgA1mWwhfvI7w3AvhQy1dW82ze1rlEXmEqbD9Dw4p5OS72T8D-NkXHuj4c2zkP8G61FbMKMuVsN3PsDe-7aMGxdlySVq-2Z-yfvebtFmb3nU_rOuVvuPObWsszPhxm9X4TQfRCVHMRV2Hdg208FJ0ErR7j8fL9AguevEWiP9mPX6C6E5Hwrp_0xDZeDvhoN9-gnr3ZgcC2V7jgltphAkPUXytG4OqdkIK77fLFCtb_3nlg45VXvouOn8u0GCtNDVrIOIViW96pkEz-f4hLZJFHkg2UKCr3adhJaHrjW6AYr83XzQSi2ED4BMg3ITFoFcqwoZdySzRp8vU6Qqqbjq-sZYzfpwTjNJaWfmf8DHHYe-SewMPpkxssQcyEyrtTlHAybQVNDF28X0firACG1sIoRPIk2STAVmFBzZyqRhQ9oM80qORBDWfCNXcfHMfkIdC6XpLspWf0M9gyN-tM8DcyJykaNmch-R-hPOkl12Kjw66me-3-FwWYs_UxiaLrHL2c9Yd9pHZv5yz0Mp-iz-pDid-P0bkJNGuvcuqHZL7QqGVS7ZmT_HwMwaRs=s640-no?authuser=1",
+    "https://lh3.googleusercontent.com/tk9PJG-WoZSiYgF6TZG7XTsuDgPQHLdB-3FmCQYnItFwXyANi3ySnICr6Q6hbu5G-3c_9ZAsaeu_t1k6W4PoBG7Zl4QfUcddEDjSxHhT6jwRNkilT5EcNzWR3vWZfXTFq2PbO7zpNMaYORAP9YUfKHf8KN1wAUPQhzbOUx5fbJXMCwlobwHSz3WvRqsqaUi38gZw2gUyCYH2KGAHg4q3wxWPYU5B_NURbFLSW3Q9ZkdEAqfsi35u6OYonsLBUuVH0uTNbbfKqjBGZeqTG9r_HAQ7pZsioJP5X7pyqcpQlakuBCqoIRnjyWSVfO2qHA0fOvWnwvnkRWmDO6xYBYuc4PbF9C1YqgMxnNpA8JvHwQVayoRlsgP1iBN3-umKPT4YpW1lMqS0ZU1yQ3Zrl80UxeTkx_aoFrdSvKdQL7Efnd3MQ32Jz5otSqqYYlLd6paDo6ViumObAbd9gV58dl6Dr23vxMTn23Z9gew7NrtJ4JPMCTyQXMqr-RBU_3_BdcesrQLWrjSc9gpBDulSh7TMvfbvC__AAkLZIx_TI_uyh9BjY2UC2qEV-4AHO-FhZYzQlDGlzq6O9_GkrugOCeVT9k202WGug2zkVkOq7u7aQ5h9MO5K9QJin4zxldLCWcleku7BPeuECPmiAdYAZgRbpnYyu2NJ2Xm2f2HMqBHxsv4YxsTSWFY1GRuSf_GkF0UJn70OjVjy2xEdYhZ4=s640-no?authuser=1",
+    "https://lh3.googleusercontent.com/4A11MODOqtFPef1fg3XFmtZVB2gv1aVw-2hgBJDdz4XVsjA-TtPbWUB4wgpzG9MZQHUKogYZa-bqh14bc0lShX1Meshk0vuX6ki7cBRrLv4WaKH4GNiSkSb06l7aeNZDWBKrQgypJp-x4HaMgL2vyJPIwM3Y68YeI3nUjXrzVn8vlR87UfOa2zMxUU55e4p-TfFJhTFrXyy-tJUMRwLQqmT6BqW3Oe5QpwqrfXTKrI463UoeBCGwbAaXEuVvQu3w4rvUDmnisqDEfA07ceNHvVmXbigP22vZ4jbjSHyn1wuemTOO5Js7P5Ms5lJogFV1nKmyddkarHgfmq5oJlIVrSlaxEuoPCxNf2q3xYUqd8AQ7ytq9ISi9_Eqzc4-7TlQfQuFi-0nwjxTuLngN38YTzQXOwPGfkrnTN3HOYxsjd6zmgZwTu0xc-bOlNv28bsoY_xKMMUuL3Ucvz8A7PHMXZ0jRpcMXJq3DrpP_TmyTVrnWo-NOxeR3wQAE5huHTv437fsy9cGgF001mFGcHv25N4vWZqMVD8WG5jEJ9grMyj2fWqwPqi4W2tkoKm-nR67vQJdHjU6Gs54Lh0fz4zIMFhl8G0BxnjD8QSENHILVVMGGqoLWGC-3ZRBjpa7IL2zvzZdYueSJQ_Bbg8p7IW8t0htBKMeKC4X2gh0Li4HQvR5ruIaww8no2Eqvx-Q_G6_MsbPNF9Mjxbv5qqz=s640-no?authuser=1"
+)
+
 ad <- data.frame(
     name = c(
         "Инвестиции USA Markets",
@@ -65,21 +72,16 @@ shinyApp(
                         title = "Наши каналы",
                         swipeToClose = TRUE,
                         closeButton = FALSE,
-                        f7Block(
-                            f7Link(ad$name[1], ad$link[1]),
-                            ad$text[1]
-                        ),
-                        f7Block(
-                            f7Link(ad$name[2], ad$link[2]),
-                            ad$text[2]
-                        ),
-                        f7Block(
-                            f7Link(ad$name[3], ad$link[3]),
-                            ad$text[3]
-                        ),
-                        f7Block(
-                            f7Link(ad$name[4], ad$link[4]),
-                            ad$text[4]
+                        f7List(
+                            mode = "media",
+                            lapply(1:4, function(img) {
+                                f7ListItem(
+                                    title = ad$name[img],
+                                    ad$text[img],
+                                    media = tags$img(src = image[img]),
+                                    href = ad$link[img]
+                                )
+                            })
                         ),
                         f7Block(
                             "смахните страницу вверх / вниз, чтобы ее закрыть"
